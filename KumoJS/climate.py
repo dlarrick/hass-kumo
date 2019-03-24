87a1908548446cf958fa8704505570d081741623
Doug Larrick <doug@parkercat.org>
2019-03-24 17:15:07 -0400

    Initial working version

---
+++ KumoJS/climate.py
@@ -0,0 +1,241 @@
+import json
+import logging
+import urllib
+import urllib.parse
+from urllib.request import urlopen
+
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
+from homeassistant.components.climate import(ClimateDevice, PLATFORM_SCHEMA)
+from homeassistant.const import (STATE_OFF)
+from homeassistant.components.climate.const import(
+    STATE_HEAT, STATE_COOL, STATE_DRY, STATE_AUTO,
+    SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE, SUPPORT_FAN_MODE, SUPPORT_SWING_MODE)
+from homeassistant.const import (
+    TEMP_CELSIUS, ATTR_TEMPERATURE)
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_NAME = 'name'
+CONF_HOST = 'host'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_NAME): cv.string,
+})
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the KumoJS thermostat."""
+
+    name = config.get(CONF_NAME)
+    host = config.get(CONF_HOST)
+
+    add_devices([KumoJSThermostat(name, host)])
+
+class KumoJSThermostat(ClimateDevice):
+    """Representation of a KumoJS Thermostat device."""
+
+    def __init__(self, name, host):
+        """Initialize the thermostat."""
+        self.host = host
+        self._name = name
+        self._current = {'temperature':  None,
+                         'swing_mode': None,
+                         'fan_mode': None,
+                         'operation': None}
+        self._target_temperature = None
+        self._state = None
+        self._operation_list = ['auto', 'heat', 'cool', 'dry', 'off']
+        self._fan_list = ['auto', 'quiet', 'low', 'powerful', 'superPowerful']
+        self._swing_list = ['auto', 'horizontal', 'midhorizontal', 'midpoint', 'midvertical',
+                            'vertical', 'swing']
+        self.data = None
+        self.update()
+
+    @property
+    def state(self):
+        """Return the current state."""
+        if self.data is not None:
+            mode = self.data['r']['indoorUnit']['status']['mode']
+            if mode == 'off':
+                self._state = STATE_OFF
+            elif mode == 'cool':
+                self._state = STATE_COOL
+            elif mode == 'heat':
+                self._state = STATE_HEAT
+            elif mode == 'auto':
+                self._state = STATE_AUTO
+            elif mode == 'dry':
+                self._state = STATE_DRY
+        else:
+            self._state = None
+        return self._state
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE |
+                SUPPORT_FAN_MODE | SUPPORT_SWING_MODE)
+
+    @property
+    def name(self):
+        """Return the name of the thermostat, if any."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement which this thermostat uses."""
+        return TEMP_CELSIUS
+
+    @property
+    def current_operation(self):
+        """Return current operation mode."""
+        if self.data is not None:
+            self._current['operation'] = self.data['r']['indoorUnit']['status']['mode']
+        else:
+            self._current['operation'] = None
+        return self._current['operation']
+
+    @property
+    def operation_list(self):
+        """Returns the list of available operation modes."""
+        return self._operation_list
+
+    @property
+    def current_fan_mode(self):
+        """Return current fan setting."""
+        if self.data is not None:
+            self._current['fan_mode'] = self.data['r']['indoorUnit']['status']['fanSpeed']
+        else:
+            self._current['fan_mode'] = None
+        return self._current['fan_mode']
+
+    @property
+    def fan_list(self):
+        """Returns the list of available operation modes."""
+        return self._fan_list
+
+    @property
+    def current_swing_mode(self):
+        """Return current swing setting."""
+        if self.data is not None:
+            self._current['swing_mode'] = self.data['r']['indoorUnit']['status']['vaneDir']
+        else:
+            self._current['swing_mode'] = None
+        return self._current['swing_mode']
+
+    @property
+    def swing_list(self):
+        """Returns the list of available operation modes."""
+        return self._swing_list
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        if self.data is not None:
+            self._current['temperature'] = self.data['r']['indoorUnit']['status']['roomTemp']
+        else:
+            self._current['temperature'] = None
+        return self._current['temperature']
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        if self.data is None:
+            self._target_temperature = None
+            return None
+
+        idumode = self.data['r']['indoorUnit']['status']['mode']
+        if idumode == 'heat':
+            mode = 'spHeat'
+        elif idumode == 'cool':
+            mode = 'spCool'
+        else:
+            self._target_temperature = None
+            return None
+
+        self._target_temperature = self.data['r']['indoorUnit']['status'][mode]
+        return self._target_temperature
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return True
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+
+        # Set requires temp in F
+        temperature_f = (temperature * 1.8) + 32
+
+        idumode = self.data['r']['indoorUnit']['status']['mode']
+        if idumode == 'heat':
+            mode = 'heat'
+        elif idumode == 'cool':
+            mode = 'cool'
+        else:
+            return
+
+        req = urllib.request.Request(
+            'http://' + self.host + ':8084/v0/room/' +
+            urllib.parse.quote(self.name) + '/' + mode +
+            '/temp/' + str(temperature_f), method='PUT')
+        response = urlopen(req)
+        self._target_temperature = temperature
+        string = response.read().decode('utf-8')
+        _LOGGER.info("KumoJS set temp response: %s", string)
+
+    def set_operation_mode(self, operation_mode):
+        """Set new target operation mode"""
+        mode = "off"
+        if operation_mode == STATE_HEAT:
+            mode = "heat"
+        elif operation_mode == STATE_COOL:
+            mode = "cool"
+        elif operation_mode == STATE_DRY:
+            mode = "dry"
+        elif operation_mode == STATE_AUTO:
+            mode = "auto"
+
+        req = urllib.request.Request(
+            'http://' + self.host + ':8084/v0/room/' +
+            urllib.parse.quote(self.name) + '/mode/' + mode,
+            method='PUT')
+        response = urlopen(req)
+        self._current['operation'] = mode
+        string = response.read().decode('utf-8')
+        _LOGGER.info("KumoJS set mode response: %s", string)
+
+    def set_swing_mode(self, swing_mode):
+        """Set new vane swing mode"""
+        req = urllib.request.Request(
+            'http://' + self.host + ':8084/v0/room/' +
+            urllib.parse.quote(self.name) + '/vent/' + swing_mode,
+            method='PUT')
+        response = urlopen(req)
+        self._current['swing_mode'] = swing_mode
+        string = response.read().decode('utf-8')
+        _LOGGER.info("KumoJS set swing mode response: %s", string)
+
+    def set_fan_mode(self, fan_mode):
+        """Set new fan speed mode"""
+        req = urllib.request.Request(
+            'http://' + self.host + ':8084/v0/room/' +
+            urllib.parse.quote(self.name) + '/speed/' + fan_mode,
+            method='PUT')
+        response = urlopen(req)
+        self._current['fan_mode'] = fan_mode
+        string = response.read().decode('utf-8')
+        _LOGGER.info("KumoJS set fan speed response: %s", string)
+
+    def update(self):
+        """Get the latest data."""
+        response = urlopen('http://' + self.host + ':8084/v0/room/' +
+                           urllib.parse.quote(self.name) + "/status")
+        string = response.read().decode('utf-8')
+        _LOGGER.info("KumoJS update: %s", string)
+        self.data = json.loads(string)
---
+++ README.md
@@ -0,0 +1,27 @@
+# Home Assistant module interfacing with KumoJS
+Home Assistant control of Mitsubishi mini-split units
+
+- For US (North American) market mini-splits with KumoCloud WiFi interface
+- Implements standard Home Assistant climate entities
+- Supports Mode (heat/cool etc.), setpoint, fan speed, and vane swing setting
+- Interfaces with [KumoJS](https://github.com/sushilks/kumojs) Node.js module, which actually communicates with the KumoCloud adapter.
+  - You should have KumoJS running as a server on a locally-accessible machine.
+  - Note: Node.js on Raspbian seems to be too old to run this server as of this writing (March 2019)
+- Based on the [InComfort](https://github.com/royduin/home-assistant-incomfort) unofficial Home Assistant module as an example
+
+## Installation
+Put the `climate.py` file in `custom_components/KumoJS` within your configuration directory. On Ubuntu or Raspbian for example: `~/.homeassistant/custom_components/KumoJS`. After that configure it in the `configuration.yaml` file:
+```
+climate:
+  - platform: KumoJS
+    name: "Master BR"
+    host: 192.168.1.123
+```
+And change the `name` and `host` as needed.
+
+## TODO
+- Auto-discovery of mini-split units from KumoJS's list
+- Reverse-engineer what KumoJS is doing into a Python module, and interface with it directly
+
+## License
+[MIT](LICENSE)
